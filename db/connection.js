9c15e890e9a3c476d99094caef5c70db86b8b478
Guilherme Garcia <gui.garcia67@gmail.com>
2022-04-02 02:34:02 -0300

    rollback files to last heroku successful build

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
-const ssl_options = process.env.PORT == 5000 ? false : { rejectUnauthorized: false };
 const connectionString = process.env.DATABASE_URL;
 const db = new Pool({
     connectionString: connectionString,
-    ssl: ssl_options
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 module.exports = db;
