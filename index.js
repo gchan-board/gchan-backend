56a9164565942039ae471d45f0c8b9a231ac6238
Guilherme Garcia <gui.garcia67@gmail.com>
2022-04-03 16:31:13 -0300

    Add documentation for messages and replies routes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const fs = require('fs'); 
 
 const unless = function (path, middleware) {
   return function (req, res, next) {
@@ -49,9 +50,9 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const jsDocsOptions = {
   swaggerDefinition: {
     info: {
-      title: 'gchan API',
+      title: 'GCHAN API',
       version: '1.0.0',
-      description: 'Auto generated documentation for the gchan API',
+      description: 'Documentation for the [gchan](https://gchan.com.br) API.\n More information in the project\'s [github repository](https://github.com/guites/gchan-backend).',
     },
   },
   apis: ['index.js'],
@@ -66,7 +67,7 @@ app.use(unless(["/videoupload", "/gifupload", "/imgupload"], fileUpload()));
 app.use(morgan("tiny"));
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN_URL, // 'https://guites.github.io',
+  origin: process.env.CORS_ORIGIN_URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -105,34 +106,74 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Default route to test API status.
+ *     responses:
+ *       200:
+ *         description: Points user to the project URL and documentation.
+ */
 app.get("/", (req, res) => {
   res.json({
-    message: "fullstack msg board.",
+    message: "This is the API for the gchan project <https://gchan.com.br>. Please visit </api-docs> for more information.",
   });
 });
 
-app.post("/", (req, res) => {
-  return res.json("lel");
-});
-
+/**
+ * @openapi
+ * /messages:
+ *   get:
+ *     description: Returns all existing posts in the database.
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/messages", async (req, res) => {
-  // const allMsgs = {};
-  // allMsgs.msgs = await messages.getAll();
-  // if(req.isAuthenticated()){
-  //   allMsgs.user = req.user;
-  // }
-  // res.json(allMsgs);
   messages.getAll().then((allMessages) => {
     res.json(allMessages);
   });
 });
 
+/**
+ * @openapi
+ * /messages/{offset}:
+ *   get:
+ *     description: Returns posts from the database, offset by {offset}, with a limited of 15.
+ *     parameters:
+ *      - in: path
+ *        name: offset
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: number of posts to be skipped when fetching from database
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/messages/:offset", async (req, res) => {
   messages.getAllOffset(req.params.offset).then((messages) => {
     res.json(messages);
   });
 });
 
+/**
+ * @openapi
+ * /message/{id}:
+ *   get:
+ *     description: Return post with an id of {id}.
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric id of post to be retrieved
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/message/:id", async (req, res) => {
   const ids = req.params.id.split(",");
   if (ids.length > 1) {
@@ -141,22 +182,72 @@ app.get("/message/:id", async (req, res) => {
   messages.getOne(req.params.id).then((message) => res.json(message));
 });
 
+/**
+ * @openapi
+ * /replies:
+ *   get:
+ *     description: Returns all existing replies in the database.
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/replies", async (req, res) => {
   replies.getAll().then((allReplies) => {
     res.json(allReplies);
   });
 });
 
-app.get("/replies/:id", async (req, res) => {
+/**
+ * @openapi
+ * /replies/{post_id}:
+ *   get:
+ *     description: Return all replies from post with an id of {post_id}.
+ *     parameters:
+ *      - in: path
+ *        name: post_id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric id of post which you want the replies of
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
+app.get("/replies/:post_id", async (req, res) => {
   replies
-    .getReplyFromMessageId(req.params.id)
+    .getReplyFromMessageId(req.params.post_id)
     .then((replies) => res.json(replies));
 });
 
+/**
+ * @openapi
+ * /reply/{id}:
+ *   get:
+ *     description: Returns reply with an id of {id}.
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric id of reply to be retrieved
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/reply/:id", async (req, res) => {
   replies.getOne(req.params.id).then((reply) => res.json(reply));
 });
 
+/**
+ * @openapi
+ * /marquee:
+ *   get:
+ *     description: Returns latest 5 created marquees in the database.
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/marquee", async (req, res) => {
   marquees.getAll().then((allMarquees) => {
     res.json(allMarquees);
@@ -176,6 +267,15 @@ app.get("/login", function (req, res, next) {
   }
 });
 
+/**
+ * @openapi
+ * /placeholders:
+ *   get:
+ *     description: Redirects to a random registered placeholder image from the database.
+ *     responses:
+ *       200:
+ *         description: Success.
+ */
 app.get("/placeholders", async function (req, res, next) {
   placeholders.getRandom().then((placeholder) => {
     const protc = req.secure ? "https://" : "http://";
@@ -185,12 +285,33 @@ app.get("/placeholders", async function (req, res, next) {
   });
 });
 
+/**
+ * @openapi
+ * /placeholders/{file}:
+ *   get:
+ *     description: Server a placeholder named {file} as an image.
+ *     parameters:
+ *      - in: path
+ *        name: file
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: file name of the image to be served
+ *     responses:
+ *       200:
+ *         description: Success.
+ *       404:
+ *          description: File not found.
+ */
 app.get("/placeholders/:file", function (req, res, next) {
-  res.sendFile(__dirname + "/placeholders/" + req.params.file);
+  const filePath = __dirname + "/placeholders/" + req.params.file;
+  if (!fs.existsSync(filePath)) {
+    res.status(404); res.json("'message': 'File not found.'");
+  }
+  res.sendFile(filePath);
 });
 
 app.post("/login", function (req, res, next) {
-  // console.log(req.body);
   passport.authenticate("local", function (err, user, info) {
     console.log(user);
     if (err) {
@@ -230,12 +351,82 @@ app.post("/register", async function (req, res, next) {
   }
 });
 
+/**
+ * @openapi
+ * /messages:
+ *   post:
+ *     description: Adds a new post.
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: post
+ *         description: The post to create.
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - message
+ *           properties:             
+ *             username:
+ *               type: string
+ *             subject:
+ *               type: string
+ *             message:
+ *               type: string
+ *             imageURL:
+ *               type: string
+ *             giphyURL:
+ *               type: string
+ *             options:
+ *               type: string
+ *             user_id:
+ *               type: integer
+ *             gif_origin:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Created.
+ */
 app.post("/messages", async (req, res) => {
+  // TODO: correctly set status codes for creation (201), recaptcha failure, validation error, etc
   messages.postMessage(req).then((message) => {
     res.json(message);
   });
 });
 
+/**
+ * @openapi
+ * /replies:
+ *   post:
+ *     description: Adds a new reply to a post.
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: reply
+ *         description: The reply to add to a post.
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - content
+ *             - message_id
+ *           properties:      
+ *             username:
+ *               type: string
+ *             content:
+ *               type: string
+ *             imageURL:
+ *               type: string
+ *             user_id:
+ *               type: integer
+ *             message_id:
+ *               type: integer
+ *     responses:
+ *       200:
+ *         description: Created.
+ */
 app.post("/replies", async (req, res) => {
   replies.postReply(req).then((reply) => {
     res.json(reply);
@@ -255,12 +446,6 @@ app.post("/slack", async (req, res) => {
   });
 });
 
-// app.use(fileUpload());
-// app.post('/imgupload', async (req, res) => {
-//   imgur.postImg(req.body).then(resp => {
-//     res.json(resp);
-//   })
-// })
 app.post("/imgupload", upload.single("image"), async (req, res) => {
   imgur.postImg(req.file.path, req.file.originalname).then((resp) => {
     res.json(resp);
@@ -297,9 +482,6 @@ app.delete("/imgur/:deletehash", (req, res) => {
     res.json(resp);
   });
 });
-// function checkAuthenticated(req, res, next){
-//   if(req.isAuthenticated)
-// }
 
 const port = process.env.PORT || 4450;
 app.listen(port, () => {
