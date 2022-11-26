1f82c4707de6d370b7067b080a9fde8ed5163092
Guilherme Garcia <gui.garcia67@gmail.com>
2022-11-26 13:26:31 -0300

    Refactor interface with imgur API

--- db/imgur.js
+++ db/imgur.js
@@ -3,80 +3,52 @@ const FormData = require('form-data');
 const fs = require("fs");
 
 async function postImg(filepath, filename) {
-
-  var formdata = new FormData();
-    if (fs.existsSync(filepath)) {
-      const imgFile = fs.readFileSync(filepath);
-      formdata.append("image", imgFile, filename);
-      var requestOptions = {
-        method: 'POST',
-        headers: {
-          Authorization: "Client-ID 3435e574a9859d1",
-        },
-        body: formdata,
-        redirect: 'follow'
-      };
-
-      return fetch("https://api.imgur.com/3/image", requestOptions)
-      .then(response => response.json())
-      .catch(error => error);
-    } else {
-      return {error: 'erro na conexão com imgur.'};
-    }
+  const form = new FormData();
+  if (!fs.existsSync(filepath)) {
+    return { status_code: 500, details: "We couldn't find your image! Please try uploading one more time." }
   }
-  
+  const imgFile = fs.readFileSync(filepath);
+  form.append("image", imgFile, filename);
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+    },
+    body: form,
+    redirect: 'follow'
+  };
 
-async function postGif(filepath, filename) {
-  var formdata = new FormData();
-    if (fs.existsSync(filepath)) {
-      //file exists
-      const gifFile = fs.readFileSync(filepath);
-      formdata.append("image", gifFile,filename);
-      var requestOptions = {
-        method: 'POST',
-        headers: {
-          Authorization: "Client-ID 3435e574a9859d1",
-        },
-        body: formdata,
-        redirect: 'follow'
-      };
-    
-      return fetch("https://api.imgur.com/3/image", requestOptions)
-      .then(response => response.json())
-      .catch(error => error);
-    } else {
-      return {error: 'erro na conexão com imgur.'};
-    }
+  return fetch("https://api.imgur.com/3/image", requestOptions)
+  .then(response => response.json())
+  .catch(error => error);
 }
 
 async function postVideo(filepath, filename) {
-  var formdata = new FormData();
-    if (fs.existsSync(filepath)) {
-      //file exists
-      const videoFile = fs.readFileSync(filepath);
-      formdata.append("video", videoFile,filename);
-      var requestOptions = {
-        method: 'POST',
-        headers: {
-          Authorization: "Client-ID 3435e574a9859d1",
-        },
-        body: formdata,
-        redirect: 'follow'
-      };
+  const formdata = new FormData();
+  if (!fs.existsSync(filepath)) {
+    return { status_code: 500, details: "We couldn't find your video! Please try uploading one more time." }
+  }
+  const videoFile = fs.readFileSync(filepath);
+  formdata.append("video", videoFile,filename);
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+    },
+    body: formdata,
+    redirect: 'follow'
+  };
     
-      return fetch("https://api.imgur.com/3/upload", requestOptions)
-      .then(response => response.json())
-      .catch(error => error);
-    } else {
-      return {error: 'erro na conexão com imgur.'};
-    }
+  return fetch("https://api.imgur.com/3/upload", requestOptions)
+  .then(response => response.json())
+  .catch(error => error);
 }
 
 async function deleteImgur(deletehash) {
 	return fetch(`https://api.imgur.com/3/image/${deletehash}`, {
         method: 'DELETE',
         headers: {
-          Authorization: 'Client-ID 3435e574a9859d1',
+          Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
         },
         redirect: 'follow',
       })
